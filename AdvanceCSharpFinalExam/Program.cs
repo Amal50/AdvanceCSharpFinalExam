ab8453df0bd5c73d6e14fe84811d463901754b22
Amal50 <smartprogs.com@gmail.com>
2020-08-15 10:33:18 -0400

    commit

--- AdvanceCSharpFinalExam/Program.cs
+++ AdvanceCSharpFinalExam/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace AdvanceCSharpFinalExam
 {
@@ -6,7 +8,67 @@ namespace AdvanceCSharpFinalExam
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World! test");
+           
+
+            //event Test
+            EventPublisher eventPublisher = new EventPublisher();
+            EventSubscriper eventSubscriper = new EventSubscriper();
+
+            eventPublisher.MyEventHandler += eventSubscriper.OnEventPublished;
+            eventPublisher.MyEventHandler += eventSubscriper.AnotherOnEventPublished;
+
+            eventPublisher.DoSomeWork("my exam work");
+            
         }
     }
+
+    public class GenericClass<T> where T : struct
+    {
+        private List<T> _collection = new List<T>();
+
+        public void Add(T parm)
+        {
+            _collection.Add(parm);
+        }
+
+        public T GetItem(int index)
+        {
+            T item = _collection[index];
+            return item;
+        }
+        public List<T> GetSorted()
+        {
+            _collection.Sort();
+            _collection.Reverse();
+            return _collection;
+            
+        }
+
+    }
+
+
+    public class EventPublisher
+    {
+        public event EventHandler<string> MyEventHandler;
+        public void DoSomeWork(string work)
+        {
+            Console.WriteLine("I'm doing some work: " + work);
+            MyEventHandler?.Invoke(this, "Event Happened");
+        }
+
+    }
+
+    public class EventSubscriper
+    {
+        public void OnEventPublished(object source, string e)
+        {
+            Console.WriteLine("OnEventPublished " + e);
+        }
+
+        public void AnotherOnEventPublished(object source, string e)
+        {
+            Console.WriteLine("AnotherOnEventPublished " + e);
+        }
+    }
+
 }
